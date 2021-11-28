fb996299e58dee4b89fc705341f6fe42f249ca2b
Gullapalli, Ishawn <ixixiyiyixixi@gmail.com>
2021-11-27 22:52:40 -0500

    Added animations

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
 $(document).ready(function() {
 
 	$(".answer__item").click(function(event) {
+		const isSelectable = !$(".answers").hasClass("answers--not_selectable");
 		const isCorrect = $(this).text().includes("Lightning");
-		if (isCorrect) {
-			$(this).addClass("answer__item--correct");
-		} else {
-			$(this).addClass("answer__item--incorrect");
-			$(".answer__item").first().addClass("answer__item--correct");
+
+		if (isSelectable) {
+			if (isCorrect) {
+				$(this).addClass("answer__item--correct");
+			} else {
+				$(this).addClass("answer__item--incorrect");
+				$(".answer__item").first().addClass("answer__item--correct");
+			}
 		}
+	});
+
+	$("#start-button").click(function(event) {
+		const questionFadeTime = 300;
+		const beforeAnswerDelay = 1500;
+		const answerFadeTime = 300;
+		const answerDelay = 90;
+
+		$(this).remove();
+		$(".question").fadeIn(questionFadeTime, function() {
+			const answer_items = $(".answer__item");
+			answer_items.each(function(index) {
+				$(this).delay(beforeAnswerDelay + answerDelay * index).fadeIn(answerFadeTime, function() {
+					if (index == answer_items.length - 1) {
+						$(".answers").removeClass("answers--not_selectable");
+					}
+				});
+			});
+		});
 	});
 
 });
