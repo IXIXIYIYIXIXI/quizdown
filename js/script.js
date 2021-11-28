032459d2f2b56d5c089b82479e49746fb5974ea5
Gullapalli, Ishawn <ixixiyiyixixi@gmail.com>
2021-11-28 05:18:04 -0500

    Parsing json

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+	$.getJSON("https://raw.githubusercontent.com/IXIXIYIYIXIXI/quizdown/main/topics/pjo.json", function(json) {
+		let questions = json["questions"];
+		shuffleArray(questions);
+		questions = questions.slice(0, 7);
+		for (const question of questions) {
+			const answers = question["answers"];
+			question["correct"] = answers[question["correct"]];
+			shuffleArray(answers);
+		}
+		console.log(questions);
+	});
+
 	$(".answer__item").click(function(event) {
 		const isSelectable = !$(".answers").hasClass("answers--not_selectable");
 		const isCorrect = $(this).text().includes("Lightning");
